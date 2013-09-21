6fa919a05ba56cfadaf372186b7df6cd89935dad
Mario Visic <mario@mariovisic.com>
2013-09-21 16:08:27 +1000

    Update monitoring to work with latest newrelic release

--- lib/newrelic-pjax/agent/monitoring.rb
+++ lib/newrelic-pjax/agent/monitoring.rb
@@ -5,37 +5,27 @@ module NewRelic
     module Agent
       module Monitoring
         include NewRelic::Agent::BrowserMonitoring
+
         def pjax_timing_start
-          unless NewRelic::Agent.instance.beacon_configuration.nil? || !NewRelic::Agent.is_transaction_traced? || !NewRelic::Agent.is_execution_traced? || NewRelic::Agent::TransactionInfo.get.ignore_end_user?
-            %{<script type="text/javascript">
-            pjaxTiming['firstByte'] = new Date().getTime();
-          </script>
-            }.html_safe
+          if insert_js?
+            %{<script type="text/javascript">pjaxTiming['firstByte'] = new Date().getTime();</script>}.html_safe
           end
         end
 
         def pjax_timing_end
           config = NewRelic::Agent.instance.beacon_configuration
 
-          if config.nil? ||
-            !config.enabled? ||
-            NewRelic::Agent.config[:browser_key].nil? ||
-            NewRelic::Agent.config[:browser_key].empty? ||
-            !NewRelic::Agent.is_transaction_traced? ||
-            !NewRelic::Agent.is_execution_traced? ||
-            NewRelic::Agent::TransactionInfo.get.ignore_end_user?
-            return ""
+          if insert_js?
+            pjax_footer_js_string(config)
           end
-
-          pjax_footer_js_string(config)
         end
 
         def pjax_footer_js_string(config)
-          if browser_monitoring_start_time
+          if current_transaction.start_time
             %{<script type="text/javascript">
             pjaxTiming['transactionName'] = '#{browser_monitoring_transaction_name}';
-            pjaxTiming['queueTime'] = #{browser_monitoring_queue_time};
-            pjaxTiming['appTime'] = #{browser_monitoring_app_time};
+            pjaxTiming['queueTime'] = #{current_timings.queue_time_in_millis};
+            pjaxTiming['appTime'] = #{current_timings.app_time_in_millis};
             pjaxTiming['lastByte'] = new Date().getTime();
           </script>}.html_safe
           else
